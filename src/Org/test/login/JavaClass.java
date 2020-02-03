e932a5ed4487b726b04bbfc6734ad7c7faa8f2a0
Ramyasara <ramyabanu1815@gmail.com>
2020-02-03 06:46:18 +0530

    Java Project 1 Added

---
+++ src/Org/test/login/JavaClass.java
@@ -0,0 +1,17 @@
+package Org.test.login;
+
+public class JavaClass {
+			public void java() {
+			System.out.println("Welcome to Java Class 1");
+			}
+			private void Oracle() {
+				System.out.println("Oracle Class not Started");
+			}
+			public static void main(String[] args) {
+				JavaClass j = new JavaClass();
+				j.java();
+				j.Oracle();
+			}
+	}
+
+
